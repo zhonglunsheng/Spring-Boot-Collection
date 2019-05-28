60155587d3f8e88ff2528795e859d809f5226831
zhonglunsheng <873946453@qq.com>
2019-05-28 13:42:16 +0800

    集成oshi获取系统信息

--- Spring-Boot-V2.x/src/test/java/com/lsheng/boo/BootVApplicationTests.java
+++ Spring-Boot-V2.x/src/test/java/com/lsheng/boo/BootVApplicationTests.java
@@ -1,5 +1,6 @@
 package com.lsheng.boo;
 
+import com.lsheng.boo.domain.Server;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class BootVApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+        Server server = new Server();
+        server.copyTo();
+        System.out.println(server.toString());
     }
 
 }
